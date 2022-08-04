eee9e329fde0a99c473a47fd11705d381e840f7d
D-Toshchakov <daniiltoshchakov@gmail.com>
2022-08-04 10:54:31 +0300

    implemented jwt auth

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/D-Toshchakov/React-JWT-tutorial/database"
 	"github.com/D-Toshchakov/React-JWT-tutorial/models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const SecretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -52,5 +58,30 @@ func Login(c *fiber.Ctx) error {
 			"message": "incorrect password",
 		})
 	}
-	return c.JSON(user)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour*24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey ))
+	if err!= nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "could not login",
+		})
+	}
+	
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour*24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "Success",
+	})
 }
--- go.mod
+++ go.mod
@@ -2,20 +2,23 @@ module github.com/D-Toshchakov/React-JWT-tutorial
 
 go 1.18
 
-require github.com/gofiber/fiber/v2 v2.36.0
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+	gorm.io/driver/mysql v1.3.5
+	gorm.io/gorm v1.23.8
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
-	gorm.io/driver/mysql v1.3.5 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
