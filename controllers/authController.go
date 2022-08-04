4b7845f2d8e51533b58958c838a0df632df0acc9
D-Toshchakov <daniiltoshchakov@gmail.com>
2022-08-04 11:51:33 +0300

    Implemented user logout

--- controllers/authController.go
+++ controllers/authController.go
@@ -83,7 +83,7 @@ func Login(c *fiber.Ctx) error {
 	c.Cookie(&cookie)
 
 	return c.JSON(fiber.Map{
-		"message": "Success",
+		"message": "Successfully logged in",
 	})
 }
 
@@ -107,4 +107,17 @@ func User(c *fiber.Ctx) error {
 	database.DB.Where("id = ?", claims.Issuer).First(&user)
 
 	return c.JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name: cookieJWT,
+		Value: "",
+		Expires: time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "Successfuly logged out",
+	})
 }
